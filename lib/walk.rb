ee92afd3ee5b444b6d418a8cd6aca64b130df488
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-11-01 10:12:04 +0000

    Added README with details

---
+++ lib/walk.rb
@@ -0,0 +1,20 @@
+def ten_minute_walk?(walk)
+  horizontal = 0 
+  vertical = 0 
+
+walk.each do |x| 
+  if x == 'n'
+    vertical += 1
+  elsif x == 's'
+    vertical -= 1
+  elsif walk == 'w'
+    horizontal += 1
+  elsif walk == 'e'
+    horizontal -= 1
+  end
+end 
+
+  count = walk.length
+  vertical == 0 && horizontal == 0 && count == 10
+
+end 
---
+++ spec/walk_spec.rb
@@ -0,0 +1,23 @@
+require 'walk'
+
+describe "return to centre in 10 min walk" do 
+
+  it 'should return false' do
+    expect(ten_minute_walk?(['n'])).to eq(false)
+  end
+
+  it 'should return false' do 
+    expect(ten_minute_walk?(['s'])).to eq(false)
+  end
+
+  it 'should return true' do 
+    expect(ten_minute_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq(true)
+  end 
+
+  it 'should return false' do 
+    expect(ten_minute_walk?(['w', 's', 'e', 'n', 'n', 'e', 's', 'w', 'w', 'w'])).to eq(false)
+  end 
+
+
+
+end
